c280529b618620ef724f5634f13688ed530654c5
BangMingGing <privatebmk802@naver.com>
2023-04-05 18:38:50 +0900

    add inference

--- Device/device.py
+++ Device/device.py
@@ -5,8 +5,6 @@ import pickle
 import time
 import subprocess
 
-sys.path.append('..')
-import utils as ut
 
 RABBITMQ_SERVER_IP = '203.255.57.129'
 RABBITMQ_SERVER_PORT = '5672'
@@ -14,18 +12,18 @@ RABBITMQ_SERVER_PORT = '5672'
 
 class Device():
     
-    def __init__(self, device_name, smartQ_id):
+    def __init__(self, device_name, smartQ_email):
         self.credentials = pika.PlainCredentials('rabbitmq', '1q2w3e4r')
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_SERVER_IP, RABBITMQ_SERVER_PORT, 'vhost', self.credentials))
         self.channel = self.connection.channel()
 
-        self.device_name = device_name
         self.queue_name = device_name
+        self.smartQ_email = smartQ_email
         
         # Queue 선언
         queue = self.channel.queue_declare(device_name)
         # Queue-Exchange Binding
-        self.channel.queue_bind(exchange=smartQ_id, queue=device_name, routing_key=f'to{device_name}')
+        self.channel.queue_bind(exchange=smartQ_email, queue=device_name, routing_key=f'to{device_name}')
 
         
 
@@ -34,19 +32,19 @@ class Device():
     def callback(self, ch, method, properties, body):
         message = pickle.loads(body, encoding='bytes')
         header = message['header']
-        message = msg['message']
-        model_name = message['model_name']
+        file_name = message['name']
         contents = message['contents']
 
         if header == 'model':
-            with open(f'{model_name}', 'wb') as f:
+            with open(f'{file_name}', 'wb') as f:
                 f.write(contents)
 
-            model = ['python', 'Inference_worker.py', model_name, 'inference_image.jpg']
+            model = ['python', 'Inference_worker.py', file_name, 'inference_image.jpg']
 
             result_message = {}
+            result_message['email'] = self.smartQ_email
             result_message['device_name'] = self.device_name
-            result_message['model_name'] = model_name
+            result_message['model_name'] = file_name
 
             start_time = time.time()
             try:
@@ -63,11 +61,11 @@ class Device():
 
             print('result : ', result_message)
 
-            self.publisher.publish(result_message)
-            os.remove(f'{model_name}')
+            self.publish(result_message)
+            os.remove(f'{file_name}')
 
         elif header == 'image':
-            with open('inference_image.jpg', 'wb') as f:
+            with open(f'{file_name}.jpg', 'wb') as f:
                 f.write(contents)
             print("image saved")
 
@@ -75,24 +73,26 @@ class Device():
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     def consume(self):
-        # self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_qos(prefetch_count=1)
         self.channel.basic_consume(on_message_callback=self.callback, queue=self.queue_name)
         print(f'[{self.device_name}] Start Consuming')
-        try:
-            self.channel.start_consuming()
-        except:
-            self.publisher = ut.Publisher(header='result', exchange_name='output', routing_key='toMongoDB')
+        self.channel.start_consuming()
+        
 
-    def publish(self):
-        return True
+    def publish(self, message):
+        self.basic_publish(
+            exchange='output',
+            routing_key='toMongoDB',
+            body=pickle.dumps(message)
+        )
         
 
 if __name__ == '__main__':
     
-    smartQ_id = sys.argv[1]
+    smartQ_email = sys.argv[1]
     device_name = sys.argv[2]
 
-    process = Device(device_name=device_name, smartQ_id=smartQ_id)
+    process = Device(device_name=device_name, smartQ_email=smartQ_email)
     process.consume()
 
     
